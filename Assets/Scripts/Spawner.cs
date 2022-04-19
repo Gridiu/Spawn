cf7d7d8523a6ce525755f9c469fb8dad3a0f29c0
Gridiu <gridin.petr@gmail.com>
2022-04-19 21:53:17 +0300

    Attempt1

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform _points;
+    [SerializeField] private GameObject _prefab;
+
+    private Transform[] _spawnPoints;
+    private int _currentPointIndex;
+
+    private void Start()
+    {
+        _spawnPoints = new Transform[_points.childCount];
+
+        for (int i = 0; i < _points.childCount; i++)
+        {
+            _spawnPoints[i] = _points.GetChild(i);
+        }
+    }
+
+    public void SpawnEnemy()
+    {
+        int firstPointIndex = 0;
+
+        Instantiate(_prefab, _spawnPoints[_currentPointIndex].transform);
+
+        _currentPointIndex++;
+
+        if (_currentPointIndex == _spawnPoints.Length)
+        {
+            _currentPointIndex = firstPointIndex;
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    private const float TimerMinValue = 0f;
+    private const float TimerMaxValue = 2f;
+
+    [SerializeField] private UnityEvent _timerReset;
+
+    private float _timer;
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= TimerMaxValue)
+        {
+            _timer = TimerMinValue;
+
+            _timerReset.Invoke();
+
+            Debug.Log(Time.time);
+        }         
+    }
+}
